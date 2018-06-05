a186c1d9d982952a5b63b37fa8c2ad836ed0bb3b
Sean Hong <iamseanhong@gmail.com>
2018-06-04 21:25:10 -0700

    Shallow mount App at beforeEach

--- test-basic/src/__test__/App.test.js
+++ test-basic/src/__test__/App.test.js
@@ -6,14 +6,16 @@ import App from '../components/App';
 import CommentBox from '../components/app/CommentBox';
 import CommentList from '../components/app/CommentList';
 
-it('has one CommentBox component', () => {
-  const component = shallow(<App />);
+let component;
 
+beforeEach(() => {
+  component = shallow(<App />);
+});
+
+it('has one CommentBox component', () => {
   expect(component.find(CommentBox).length).toEqual(1);
 });
 
 it('has one CommentList component', () => {
-  const component = shallow(<App />);
-
   expect(component.find(CommentList).length).toEqual(1);
 });
