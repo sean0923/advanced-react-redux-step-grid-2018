c0185ee1842dc1e8ffd0691198a9ab4a54017b3c
Sean Hong <iamseanhong@gmail.com>
2018-06-04 21:44:33 -0700

    Add handleer at CommentBox

--- test-basic/src/components/app/CommentBox.js
+++ test-basic/src/components/app/CommentBox.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CommentBox = () => {
-  return (
-    <div>
-      <div>CommentBox</div>
-    </div>
-  );
-};
+class CommentBox extends Component {
+  state = {
+    userInput: '',
+  };
+
+  handleOnChange = e => {
+    this.setState({ userInput: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ userInput: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Comment</h4>
+        <textarea value={this.state.userInput} onChange={this.handleOnChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default CommentBox;
