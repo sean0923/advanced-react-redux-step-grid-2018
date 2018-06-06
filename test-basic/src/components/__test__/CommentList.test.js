be93434fb8b7daab9c6c5d05ddf13fff65b29682
Sean Hong <iamseanhong@gmail.com>
2018-06-06 00:36:10 -0700

    Test redux store with react

--- test-basic/src/components/__test__/CommentList.test.js
+++ test-basic/src/components/__test__/CommentList.test.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Root from 'Root';
 import CommentList from '../app/CommentList';
 
 let component;
+let initialState;
 
 beforeEach(() => {
-  component = shallow(
-    <Root>
+  initialState = {
+    comments: ['comment1', 'comment2'],
+  };
+
+  component = mount(
+    <Root initialState={initialState}>
       <CommentList />
     </Root>
   );
 });
 
 it('has same number of <li> as comments', () => {
-  expect(1).toEqual(2);
+  expect(component.find('li').length).toEqual(2);
+});
+
+it('has same number of <li> as comments', () => {
+  // component.render().text()
+  const allText = component.render().text();
+  expect(allText).toContain('comment1');
+  expect(allText).toContain('comment2');
 });
