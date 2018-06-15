3187c5549b0d223dbd397bc2b6355f1c3dedbde9
Sean Hong <iamseanhong@gmail.com>
2018-06-14 21:24:01 -0700

    Able to create user with postman

--- auth/controllers/authentication.js
+++ auth/controllers/authentication.js
@@ -1,3 +1,22 @@
+const Users = require('../models/users');
+
 exports.signup = (req, res, next) => {
-  res.send({ success: true });
+  const { email, password } = req.body;
+
+  Users.findOne({ email })
+    .then(user => {
+      if (user === null) {
+        const user = new Users({ email, password });
+        return user.save();
+      } else {
+        res.send('user already exist');
+      }
+    })
+    .then(newUser => {
+      res.send(newUser);
+    })
+    .catch(err => {
+      console.log('err: ', err);
+      res.send(err);
+    });
 };
--- auth/server/routes.js
+++ auth/server/routes.js
@@ -2,11 +2,4 @@ const Authentication = require('../controllers/authentication');
 
 module.exports = app => {
   app.post('/signup', Authentication.signup);
-  // app.get('/', (req, res, next) => {
-  //   res.send([1, 2, 3]);
-  // });
-
-  // app.get('*', (req, res) => {
-  //   res.send('working?');
-  // });
 };
