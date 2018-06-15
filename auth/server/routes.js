7c05f4f216b4f7e8638a2272406ccfeeb7231a75
Sean Hong <iamseanhong@gmail.com>
2018-06-14 20:53:01 -0700

    Add authentication controller

--- auth/server/routes.js
+++ auth/server/routes.js
@@ -1,9 +1,12 @@
-module.exports = app => {
-  app.get('/', (req, res, next) => {
-    res.send([1, 2, 3]);
-  });
+const Authentication = require('../controllers/authentication');
 
-  app.get('*', (req, res) => {
-    res.send('working?');
-  });
+module.exports = app => {
+  app.post('/signup', Authentication.signup);
+  // app.get('/', (req, res, next) => {
+  //   res.send([1, 2, 3]);
+  // });
+
+  // app.get('*', (req, res) => {
+  //   res.send('working?');
+  // });
 };
