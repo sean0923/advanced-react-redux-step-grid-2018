01551693787567b957da5acf20ac83b6debc4be8
Sean Hong <iamseanhong@gmail.com>
2018-06-17 00:17:50 -0700

    Render Signup component

---
+++ auth/client/src/components/pages/Signup.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignInAndOut from '../common/SignInAndOut';
+
+const Signup = () => {
+  return <SignInAndOut />;
+};
+
+export default Signup;
--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -3,12 +3,16 @@ import ReactDom from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/App';
+
 import Welcome from './components/Welcome';
+
+import Signup from './components/pages/Signup';
 
 ReactDom.render(
   <BrowserRouter>
     <App>
       <Route path="/" exact component={Welcome} />
+      <Route path="/signup" exact component={Signup} />
     </App>
   </BrowserRouter>,
   document.getElementById('root')
