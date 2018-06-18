8435c05ed8b5102c6879fb8293fc6555c89adb7d
Sean Hong <iamseanhong@gmail.com>
2018-06-17 17:40:00 -0700

    Get token from local storage

--- auth/client/src/ProviderWithStore.js
+++ auth/client/src/ProviderWithStore.js
@@ -4,13 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
-const ProviderWithStore = ({ initialState = {}, children }) => {
+const ProviderWithStore = ({
+  initialState = { auth: { jwtToken: localStorage.getItem('token') } },
+  children,
+}) => {
   const store = createStore(rootReducer, initialState, applyMiddleware(reduxThunk));
-  return (
-    <Provider store={store} >
-      {children}
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export default ProviderWithStore;
--- auth/client/src/reducers/authReducer.js
+++ auth/client/src/reducers/authReducer.js
@@ -8,6 +8,7 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.GET_JWT_TOKEN_FROM_SERVER:
+      localStorage.setItem('token', action.payload.data.jwtToken);
       return { jwtToken: action.payload.data.jwtToken, errorMessage: '' };
 
     case types.GOT_ERROR_MSSG_FROM_SERVER:
